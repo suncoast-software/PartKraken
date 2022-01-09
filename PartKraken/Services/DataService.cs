e6fdd329c3dfeb7fb3a0d9f709bd787b48634d9f
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2022-01-09 07:05:59 -0500

    Added IDAtaService Interface and data class

---
+++ PartKraken/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿using PartKraken.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartKraken.Services
+{
+    internal class DataService : IDataService
+    {
+        public string GetConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
