ee3501c201c9b0c2c838131cefe07ec62ec68a6e
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2022-01-09 06:24:32 -0500

    add app db context factory

---
+++ PartKraken/Data/Factories/AppDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartKraken.Data.Factories
+{
+    internal class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
